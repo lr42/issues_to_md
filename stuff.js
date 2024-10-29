2ae80eafb6aa39ebe84f3899a6062316b358467a
LR <noreply_719@symfu.com>
2024-10-29 03:29:04 -0400

    Use JavaScript; Only do necessary updates
    I tried filtering for only new updates using `jq`, but it quickly go
    complicated enough that I figured I may as well just do it in
    JavaScript, so here we are.
    
    As a bonus, I was able to work on several other issues:
    
    - #1: Save issues as JSON
        - I do this now automatically.  I download the JSON and work off of
          the local file.  As a bonus, I realized that I don't need to hit
          the API endpoint for most (any?) of the information I actually
          needed, so it saves calls to the API.  I am technically
          downloading the whole data set, but only updating the files that
          need it.
    - #7: Add `vim` modelines, to set text wrapping
        - This was something that was helpful during testing, and it's just
          helpful in general.  Glad to have this included.
    - #9: Only do necessary updates
        - See #2 above.
    - #10: Remove extra lines of white space from bodies and comments
        - Easily done in JavaScript.  (Although it removes extra spaces at
          the beginning as well as newlines, but I'm OK with that.  I've
          adjusted my template accordingly.)
    
    It also partially solves some other issues:
    
    - #5: Provide a notification on new issues
        - It doesn't distinguish between new issues and existing issues that
          have been updated, but it will show you what it is updating
          (including new issues), so there's that.
    - #6: Handle nameless authors in the template
        - I just defaulted to showing the login name for now.  I'll work on
          showing the full name (if avaliable) later.

---
+++ stuff.js
@@ -0,0 +1,52 @@
+#!/bin/env node
+
+const { execSync } = require('child_process');
+const fs = require('node:fs');
+
+const mustache = require('mustache')
+
+
+// TODO Test for `git` and `gh`...
+//   Tell the user how to install them if they are not installed.
+
+execSync('gh issue list --json assignees,closed,closedAt,id,isPinned,labels,milestone,projectCards,reactionGroups,stateReason,number,title,url,state,author,createdAt,updatedAt,body,comments > ./00-issues/.issues.json')
+// `projectItems` is not accessible without setting up tokens specially.
+
+const data = JSON.parse(fs.readFileSync('./00-issues/.issues.json'));
+//console.log(data);
+
+last_update = new Date(Number(fs.readFileSync('00-issues/.last_update')) * 1000);
+//console.log(last_update);
+
+const mustache_template = fs.readFileSync('00-issues/.issue_template.mustache').toString();
+//console.log(mustache_template);
+
+for (let i of data) {
+	let updatedAt = new Date(i.updatedAt)
+	if (updatedAt > last_update) {
+		//console.log(i.number);
+		//console.log(i.title);
+		console.log(mustache.render("Issue {{number}}: {{{title}}}", i));
+
+		i.body = i.body.trim();
+		for (let c of i.comments) {
+			c.body = c.body.trim();
+		}
+
+		let output = mustache.render(mustache_template, i);
+		output = output.trim();
+		//console.log('\n *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *\n');
+		//console.log(output);
+
+		const padded_number = i.number.toString().padStart(2, '0');
+		// TODO Make `filename_safe_title` actually safe to use in filenames.
+		const filename_safe_title = i.title;
+		// TODO Get the filename format from a mustache template.
+		const filename = `${padded_number} - ${filename_safe_title}`
+		fs.writeFileSync(`00-issues/${filename}.md`, output);
+	}
+}
+
+const now = new Date();
+fs.writeFileSync('00-issues/.last_update', now.getTime().toString());
+
